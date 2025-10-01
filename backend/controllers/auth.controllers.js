2561f74657bb64ff55e4df6f87be43a42c0507c8
Vikas Choudhary <vikashbbn82@gmail.com>
2025-10-01 17:37:36 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -27,7 +27,7 @@ export const signUp = async (req, res)=>{
         res.cookie("token", token, {
             httpOnly: true,
   maxAge: 7 * 24 * 60 * 60 * 1000,
-  sameSite: "None,
+  sameSite: "None",
   secure: true,
         })
 
@@ -59,9 +59,9 @@ export const login = async (req, res)=>{
 
         res.cookie("token", token, {
             httpOnly: true,
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-  sameSite:"None",
-  secure: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite:"None",
+            secure: true,
         })
 
         return res.status(200).json(user);
